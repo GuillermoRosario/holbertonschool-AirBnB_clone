002023b5f963dee69e4aa0e9c148491a688cade8
GuillermoRosario <guillermorosario628@gmail.com>
2023-02-18 19:15:21 -0400

    Directory Check

--- models/base_model.py
+++ models/base_model.py
@@ -1,21 +1,32 @@
 #!/usr/bin/python3
 """
-Base Class Module 
+BaseModel class module
 """
 import uuid
 from datetime import datetime
 import models
 
+
 class BaseModel:
     """
-    Base Class Model that defines
-    all common atributes/ methods
+    Defines all common attributes/methods
+    for other classes
     """
-    def __init__ (self, *args, **kwargs):
+
+    def __init__(self, *args, **kwargs):
+        """
+        Initialize instance attributes
+            Attrs:
+                id (str): instance identity
+                created_at: date of instance creation
+                updated_at: date of instance attrs change
+        """
         if kwargs:
-            for key, value in kwargs.items():
-                if key != '__class__':
-                    self.__dict__[key] = value
+            for keys, value in kwargs.items():
+                if keys in ['created_at', 'updated_at']:
+                    self.__dict__[keys] = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                elif keys != '__class__':
+                    self.__dict__[keys] = value
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -24,21 +35,21 @@ class BaseModel:
 
     def __str__(self):
         """
-        Return String Rep of Instance
+        Returns string representation of instance
         """
-        return f'[{self.__class__.__name__}]({self.id}{self.__dict__})'
-    
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
     def save(self):
         """
-        Change the time of instance attr change
+        Changes the time of instance attrs change
         """
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
         """
-        Return a dictionary with all 
-        keys and values of __dict__ including new attrs
+        Returns a dictionary containing all keys/values
+        of __dict__ of the instance including new attrs
         """
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
