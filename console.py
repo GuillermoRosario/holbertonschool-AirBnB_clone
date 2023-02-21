ef46bde02e3c2197e5bdaba1604b40068ea43a3e
GuillermoRosario <guillermorosario628@gmail.com>
2023-02-21 17:46:09 -0400

    Fix documentation

--- console.py
+++ console.py
@@ -6,26 +6,27 @@ Command Interpreter OFFICIAL
 import cmd
 
 class HBNBCommand(cmd.Cmd):
+    """This is my HNBB Class"""
 
     prompt = '(hbnb) '
 
     def do_quit(self, arg):
-        'Quit command to exit the program\n'
+        """Quit command to exit the program\n"""
         return True
     
     def close(self):
-        'Close command'
+        """Close command"""
         return True
 
     def do_EOF(self, arg):
-        'EOF command to exit the program'
+        """EOF command to exit the program"""
         self.close()
         print()
         quit()
 
     def emptyline(self):
-        'Do nothing on empty line'
-    pass
+        """Do nothing on empty line"""
+        pass
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
