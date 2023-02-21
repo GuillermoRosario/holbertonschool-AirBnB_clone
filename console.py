b2c5f64267ccdd86b42c59f7326177f3e8722c0f
GuillermoRosario <guillermorosario628@gmail.com>
2023-02-21 18:30:18 -0400

    New Try

--- console.py
+++ console.py
@@ -1,27 +1,25 @@
-#!/usr/bin/python3
-"""
-Command Interpreter Interface
-"""
+#!/usr/bin/env python3
+"""Entry point of the commamd interpreter"""
+
 import cmd
 
 
 class HBNBCommand(cmd.Cmd):
-    prompt = '(hbnb) '
+    """Command Interpreter class"""
+
+    prompt = "(hbnb) "
 
     def do_quit(self, arg):
-        'Quit command to exit the program\n'
-        return True
-
-    def close(self):
+        """Quit command to exit the program"""
         return True
 
     def do_EOF(self, arg):
-        'EOF command to exit the program'
-        self.close()
+        """EOF command to exit the program"""
         print()
-        quit()
+        return True
 
     def emptyline(self):
+        """Do nothing on empty line"""
         pass
 
 if __name__ == '__main__':
