ba359e25f6e5f9cf7bbee4b475f83616b84801a4
GuillermoRosario <guillermorosario628@gmail.com>
2023-02-21 16:42:22 -0400

    Consle indent fix

--- console.py
+++ console.py
@@ -1,31 +1,31 @@
 #!/usr/bin/python3
-
 """
 Command Interpreter
 """
+
 import cmd 
-from models.base_model import BaseModel
-from models import storage
 
 class HBNBCommand(cmd.Cmd):
 
     prompt = '(hbnb) '
 
-def do_quit(self, arg):
-    'Quit command to exit the program\n'
-    return True
+    def do_quit(self, arg):
+        'Quit command to exit the program\n'
+        return True
+    
+    def close(self):
+        'Close command'
+        return True
 
-def close(self):
-    return True
+    def do_EOF(self, arg):
+        'EOF command to exit the program'
+        self.close()
+        print()
+        quit()
 
-def do_EOF(self, arg):
-    'EOF command to exit the program'
-    self.close()
-    print()
-    quit()
-
-def emptyline(self):
-        pass
+    def emptyline(self):
+        'Do nothing on empty line'
+    pass
 
 if __name__ == '__main__':
-    HBNBCommand().cmdloop()   
+    HBNBCommand().cmdloop()
