11ae4a0cf83491f9efc209aadf249ee0d006dbba
GuillermoRosario <guillermorosario628@gmail.com>
2023-02-21 15:39:28 -0400

    Console Init

---
+++ console.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""
+Command Interpreter
+"""
+
+import cmd 
+from models.base_model import BaseModel
+from models import storage
+
+class HBNBCommand(cmd.Cmd):
+
+    prompt = '(hbnb) '
+
+def quit(self, arg):
+    'Quit command to exit the program\n'
+    return True
+
+def close(self):
+    return True
+
+def EOF(self,arg):
+    'EOF command to exit the program'
+    self.close()
+    print()
+    quit()
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()   
